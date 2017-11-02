364a6f835bf76a3e2f6f0213aadaa0c4f4d0844f
Ilya Puchka <ilyapuchka@gmail.com>
2017-11-02 01:29:48 +0100

    removed unneeded code

--- DeeperTests/DeeperTests.swift
+++ DeeperTests/DeeperTests.swift
@@ -41,13 +41,11 @@ class DeeperTests: XCTestCase {
         AssertMatches(route, "http://recipes")
         AssertNotMatch(route, "http://recipe")
         AssertNotMatch(route, "http://recipe/archive")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = "recipes/archive"
         AssertMatches(route, "http://recipes/archive")
         AssertNotMatch(route, "http://recipes/archives")
         AssertNotMatch(route, "http://recipes")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
     }
 
     func testThatPatternMatchesParameter() {
@@ -57,22 +55,18 @@ class DeeperTests: XCTestCase {
         // though this pattern is not likely to be used in real scenarios
         route = "" / .recipeId
         AssertMatches(route, "http://123", params: [.recipeId: "123"])
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = .recipes / .menuId / .recipeId
         AssertMatches(route, "http://recipes/123/456", params: [.menuId: "123", .recipeId: "456"])
         AssertNotMatch(route, "http://recipes/abc/456")
         AssertNotMatch(route, "http://recipe/123")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = .recipes / ":num()" / .recipeId
         AssertMatches(route, "http://recipes/123/456", params: [.recipeId: "456"])
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = .recipeId / .recipes
         AssertMatches(route, "http://123/recipes", params: [.recipeId: "123"])
         AssertNotMatch(route, "http://123/recipe")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
     }
     
     func testThatPatternMatchesCondition() {
@@ -82,13 +76,11 @@ class DeeperTests: XCTestCase {
         AssertMatches(route, "http://recipe")
         AssertMatches(route, "http://recipes")
         AssertNotMatch(route, "http://recipes/archive")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = ("recipe" | "recipes") / "archive"
         AssertMatches(route, "http://recipe/archive")
         AssertMatches(route, "http://recipes/archive")
         AssertNotMatch(route, "http://recipes/archives")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = ("recipe" | "recipes" | "recipes" / "archive") / .recipeId
         AssertMatches(route, "http://recipe/123", params: [.recipeId: "123"])
@@ -96,12 +88,10 @@ class DeeperTests: XCTestCase {
         AssertMatches(route, "http://recipes/123", params: [.recipeId: "123"])
         AssertNotMatch(route, "http://recipes/archive/id/123")
         AssertNotMatch(route, "http://recipes/all/123")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = (("recipe" / .recipeId) | ("recipes" / .recipeId / "details"))
         AssertMatches(route, "http://recipe/123", params: [.recipeId: "123"])
         AssertMatches(route, "http://recipes/123/details", params: [.recipeId: "123"])
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
     }
     
     func testThatPatternMatchesMaybe() {
@@ -112,12 +102,10 @@ class DeeperTests: XCTestCase {
         AssertMatches(route, "http://recipe/archive")
         AssertNotMatch(route, "http://recipe")
         AssertNotMatch(route, "http://recipe/archive/data")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = "recipe" /? ("details" / .recipeId)
         AssertMatches(route, "http://recipe/details/123", params: [.recipeId: "123"])
         AssertMatches(route, "http://recipe/details")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = "recipe" /? ("details" | "detail") / .recipeId
         AssertMatches(route, "http://recipe/details/123", params: [.recipeId: "123"])
@@ -125,13 +113,11 @@ class DeeperTests: XCTestCase {
         AssertMatches(route, "http://recipe/123", params: [.recipeId: "123"])
         AssertNotMatch(route, "http://recipe/archive/123")
         AssertNotMatch(route, "http://recipe/details/detail/123")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = "recipe" /? "details" /? "detail" / .recipeId
         AssertMatches(route, "http://recipe/details/123", params: [.recipeId: "123"])
         AssertMatches(route, "http://recipe/detail/123", params: [.recipeId: "123"])
         AssertMatches(route, "http://recipe/details/detail/123", params: [.recipeId: "123"])
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
     }
 
     func testThatPatternMatchesAny() {
@@ -140,49 +126,40 @@ class DeeperTests: XCTestCase {
         route = "recipe" / .any
         AssertMatches(route, "http://recipe/123")
         AssertNotMatch(route, "http://recipe")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = .any / "recipe"
         AssertMatches(route, "http://some/recipe")
         AssertNotMatch(route, "http://recipe")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = .recipeId / .any / "recipe"
         AssertMatches(route, "http://123/recipe/recipe", params: [.recipeId: "123"])
         AssertNotMatch(route, "http://123/recipe")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = "recipe" / .any / "details"
         AssertMatches(route, "http://recipe/archive/details")
         AssertMatches(route, "http://recipe/archive/123/details")
         AssertNotMatch(route, "http://recipe/details")
         AssertNotMatch(route, "http://recipe/archive/details/123")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = "recipe" / .any / "details" / .any
         AssertMatches(route, "http://recipe/archive/details/123")
         AssertNotMatch(route, "http://recipe/archive/details")
         AssertNotMatch(route, "http://recipe/archive")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = "recipe" / .any / ("details" / "recipe" | "view") / .any
         AssertMatches(route, "http://recipe/archive/details/recipe/123")
         AssertMatches(route, "http://recipe/archive/view/123")
         AssertNotMatch(route, "http://recipe/archive/details/123")
         AssertNotMatch(route, "http://recipe/archive/overview/123")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = "recipe" / .any / "details" / .recipeId
         AssertMatches(route, "http://recipe/archive/details/123", params: [.recipeId: "123"])
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = "recipe" / .any / .recipeId
         AssertNotMatch(route, "http://recipe/archive/details/123", "any can be used only in the end or between two string patterns")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
 
         route = "recipe" / .any / .any
         AssertNotMatch(route, "http://recipe/archive/details/123", "can't have two any next to each other")
-        XCTAssertEqual(parsePattern(route.rawValue), route.pattern)
     }
     
     func testThatPatternMatchesQueryParameters() {
@@ -195,27 +172,22 @@ class DeeperTests: XCTestCase {
         AssertNotMatch(route, "http://recipe?recipeId=1")
         AssertNotMatch(route, "http://recipe?menuId=2")
         AssertNotMatch(route, "http://recipe?recipeId=1&orderId=2")
-        XCTAssertEqual(parsePattern(route.route.rawValue), route.route.pattern)
 
         route = "recipe" .?? .recipeId & .menuId
         AssertMatches(route, "http://recipe?recipeId=1&menuId=2", params: [.recipeId: "1", .menuId: "2"])
         AssertMatches(route, "http://recipe?menuId=2", params: [.menuId: "2"])
-        XCTAssertEqual(parsePattern(route.route.rawValue), route.route.pattern)
 
         route = "recipe" .? .recipeId &? .menuId
         AssertMatches(route, "http://recipe?recipeId=1&menuId=2", params: [.recipeId: "1", .menuId: "2"])
         AssertMatches(route, "http://recipe?recipeId=1", params: [.recipeId: "1"])
-        XCTAssertEqual(parsePattern(route.route.rawValue), route.route.pattern)
 
         route = "recipe" .?? .recipeId &? .menuId
         AssertMatches(route, "http://recipe")
-        XCTAssertEqual(parsePattern(route.route.rawValue), route.route.pattern)
 
         route = "recipe" .? (.recipeId | .menuId)
         AssertMatches(route, "http://recipe?recipeId=1", params: [.recipeId: "1"])
         AssertMatches(route, "http://recipe?menuId=2", params: [.menuId: "2"])
         AssertNotMatch(route, "http://recipe")
-        XCTAssertEqual(parsePattern(route.route.rawValue), route.route.pattern)
     }
     
     func testStringToPatternConversion() {
@@ -247,8 +219,6 @@ class DeeperTests: XCTestCase {
         
         XCTAssertEqual(pattern, expectedPattern)
         XCTAssertEqual(query, expectedQuery)
-        let parsed = parsePattern(stringFormat)
-        XCTAssertEqual(parsed, expectedPattern)
     }
     
     func testParamTypeValidation() {
